6eca1569868bc72c11a26ea07ad185e44eae6a2e
congkxtb98 <congkxtb98@gmail.com>
2020-06-21 19:25:25 +0700

    update

--- public/js/onOff.js
+++ public/js/onOff.js
@@ -1,99 +1,62 @@
-$(document).ready(function () {
-    let json = {
-        LED1: 0,
-        LED2: 0,
-        LED3: 0,
-        LED4: 0,
-    }
 
-    //Bong so 1 code o day
-    $(".fa-lightbulb-o.bong1").unbind("click").on("click", function () {
-        var trangthai = $(this).data('ttb1');
-        if (trangthai == 0) {
-            $(this).addClass("active");
-            $(this).data('ttb1', 1);
-            json.LED1 = 1;
-            socket.emit("LED1", json);
-
-
-        }
-        else if (trangthai == 1) {
-            $(this).removeClass("active");
-            $(this).data('ttb1', 0);
-            json.LED1 = 0;
-            socket.emit("LED1", json);
-
-
-        }
-    })
-
-    //Bong so 2 code o day
-    $(".fa-lightbulb-o.bong2").unbind("click").on("click", function () {
-        var trangthai = $(this).data('ttb1');
-
-
-
-        if (trangthai == 0) {
-            $(this).addClass("active");
-            $(this).data('ttb1', 1);
-            json.LED2 = 1;
-            socket.emit("LED2", json);
-
-        }
-        else if (trangthai == 1) {
-            $(this).removeClass("active");
-            $(this).data('ttb1', 0);
-            json.LED2 = 0;
-            socket.emit("LED2", json);
-
-
-        }
-    })
-
-
-    //Bong so 3 code o day
-    $(".fa-lightbulb-o.bong3").unbind("click").on("click", function () {
-        var trangthai = $(this).data('ttb1');
-
-
-
-        if (trangthai == 0) {
-            $(this).addClass("active1");
-            $(this).data('ttb1', 1);
-            json.LED3 = 1;
-            socket.emit("LED3", json);
-
-        }
-        else if (trangthai == 1) {
-            $(this).removeClass("active1");
-            $(this).data('ttb1', 0);
-            json.LED3 = 0;
-            socket.emit("LED3", json);
-
-
-        }
-    })
-
-    //Bong so 4 code o day
-    $(".fa-lightbulb-o.bong4").unbind("click").on("click", function () {
-        var trangthai = $(this).data('ttb1');
-
-
-
-        if (trangthai == 0) {
-            $(this).addClass("active1");
-            $(this).data('ttb1', 1);
-            json.LED4 = 1;
-            socket.emit("LED4", json);
-
-        }
-        else if (trangthai == 1) {
-            $(this).removeClass("active1");
-            $(this).data('ttb1', 0);
-            json.LED4 = 0;
-            socket.emit("LED4", json);
-
-
-        }
-    })
+$("#b1").bind("mousemove", function () {
+    move1()
 })
+$("#b2").bind("mousemove", function () {
+    move2()
+})
+$("#b3").bind("mousemove", function () {
+    move3()
+})
+$("#b4").bind("mousemove", function () {
+    move4()
+})
+$("#btn-settime1").bind("mousemove", function () {
+    $(this).addClass("runlungtung")
+    $("#settime1").addClass("hides")
+})
+$("#btn-settime2").bind("mousemove", function () {
+    $(this).addClass("runlungtung")
+    $("#settime2").addClass("hides")
+})
+$("#btn-settime3").bind("mousemove", function () {
+    $(this).addClass("runlungtung")
+    $("#settime3").addClass("hides")
+})
+$("#btn-settime4").bind("mousemove", function () {
+    $(this).addClass("runlungtung")
+    $("#settime4").addClass("hides")
+})
+function move1() {
+    var x = Math.random() * 250
+    var y = Math.random() * 250
+    var left = x + 'px'
+    var top = y + 'px'
+    document.getElementById('b1').style.left = left
+    document.getElementById('b1').style.top = top
+}
+function move2() {
+    var x = Math.random() * 250
+    var y = Math.random() * 250
+    var left = x + 'px'
+    var top = y + 'px'
+    document.getElementById('b2').style.left = left
+    document.getElementById('b2').style.top = top
+}
+function move3() {
+    var x = Math.random() * 250
+    var y = Math.random() * 250
+    var left = x + 'px'
+    var top = y + 'px'
+    document.getElementById('b3').style.left = left
+    document.getElementById('b3').style.top = top
+}
+function move4() {
+    var x = Math.random() * 250
+    var y = Math.random() * 250
+    var left = x + 'px'
+    var top = y + 'px'
+    document.getElementById('b4').style.left = left
+    document.getElementById('b4').style.top = top
+}
+
